53e90dfcf6f9d796645f8e5c5f139c36a1a04dd6
406-22-Sadanan <54472@yupparaj.ac.th>
2026-08-20 22:50:54 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -22,7 +22,7 @@ def reset_game():
 # 📌 ฟังก์ชัน MessageBox (Dialog) 
 # ---------------------------------------------------- 
 @st.dialog("📊 สรุปผลการเล่นเกม") 
-def show_result_dialog(ans1, ans2, ans3, ans4): 
+def show_result_dialog(ans1, ans2): 
     st.balloons() 
     score = 0 
  
@@ -47,7 +47,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
  
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน") 
    
-    if score == 4:
+    if score == 2:
         st.success("🎉 You win!") 
     else: 
         st.error("💀 You lose!") 
